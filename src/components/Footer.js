a134337f813d5baf6b49743b57c7a3314b88f9d8
Yashwanth <yashwanth.1341@gmail.com>
2024-05-08 21:07:40 +0530

    updated code

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,16 +6,16 @@ export const Footer = () => {
           <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <Link to="/" className="hover:underline">PlayPause</Link>. All Rights Reserved.</span>
           <ul className="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
               <li>
-                  <a href="https://www.linkedin.com/in/yashwanth-prasad-b-l-583804193/" target="_blank" className="mr-4 hover:underline md:mr-6 ">LinkedIn</a>
+                  <a href="https://www.linkedin.com/in/yashwanth-prasad-b-l-583804193/" target="" className="mr-4 hover:underline md:mr-6 ">LinkedIn</a>
               </li>
               <li>
-                  <a href="https://takeuforward.org/" target="_blank" className="mr-4 hover:underline md:mr-6">Learn DSA</a>
+                  <a href="https://takeuforward.org/" target="" className="mr-4 hover:underline md:mr-6">Learn DSA</a>
               </li>
               <li>
-                  <a href="https://flowbite.com/" target="_blank" className="mr-4 hover:underline md:mr-6">Flowbite</a>
+                  <a href="https://flowbite.com/" target="" className="mr-4 hover:underline md:mr-6">Flowbite</a>
               </li>
               <li>
-                  <a href="https://github.com/yashwanth-1341/React-Projects" target="_blank" className="hover:underline">Github</a>
+                  <a href="https://github.com/yashwanth-1341/React-Projects" target="" className="hover:underline">Github</a>
               </li>
           </ul>
       </footer>
